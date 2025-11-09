71ea3aef96cdc82a81bd27ff61484e98a2f64658
Ali Omran <ali.omran.24@lehre.mosbach.dhbw.de>
2025-11-09 14:25:33 +0100

    Add Login and SignUp screens; implement input handling and navigation

---
+++ onomis/components/BackButton.tsx
@@ -0,0 +1,37 @@
+import { StyleSheet, TouchableOpacity } from "react-native";
+import React from "react";
+import { router, Router, useRouter } from "expo-router";
+import { colors, radius } from "@/constants/theme";
+import { CaretLeft } from "phosphor-react-native";
+import { verticalScale } from "@/utils/styling";
+import { BackButtonProps } from "@/types";
+
+const BackButton = ({
+    style,
+    iconSize = 26
+}: BackButtonProps) => {
+  return (
+    <TouchableOpacity
+      onPress={() => router.back()}
+      style={[styles.button, style]}
+    >
+      <CaretLeft
+        size={verticalScale(iconSize)}
+        color={colors.white}
+        weight="bold"
+      />
+    </TouchableOpacity>
+  )
+}
+
+export default BackButton
+
+const styles = StyleSheet.create({
+    button: {
+    backgroundColor: colors.neutral600,
+    alignSelf: "flex-start",
+    borderRadius: radius._12,
+    borderCurve: "continuous",
+    padding: 5,
+  },
+})
